62354e91538e76cf070f7531d766851e9c95aeec
SunilSunil <31619543+SunilSunil@users.noreply.github.com>
2023-01-01 02:55:14 -0500

    Update sdm660.mk
    removed Snap and android.hardware.drm@1.3-service.clearkey

--- sdm660.mk
+++ sdm660.mk
@@ -121,8 +121,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
-PRODUCT_PACKAGES += \
-    Snap
+#PRODUCT_PACKAGES += \
+#    Snap
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5:64 \
@@ -180,7 +180,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+#    android.hardware.drm@1.3-service.clearkey
 
 # FM
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
